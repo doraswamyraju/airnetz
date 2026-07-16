590cb87e875318f33d51d5a65cd7c40c22005b21
Doraswamy Raju Meesala <doraswamyrajumeesala@Doraswamys-MacBook-Air.local>
2026-07-16 16:39:25 +0530

    fix: remove React.FC annotations to resolve editor type errors

--- pages/PrivacyPolicy.tsx
+++ pages/PrivacyPolicy.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Shield, Lock, Eye, Mail, Phone, MapPin, AlertTriangle } from 'lucide-react';
 
-const PrivacyPolicy: React.FC = () => {
+const PrivacyPolicy = () => {
   return (
     <div className="min-h-screen bg-gray-50 pt-24 pb-12">
       <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
--- pages/TermsOfUse.tsx
+++ pages/TermsOfUse.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FileText, ShieldAlert, CheckCircle, Scale, Mail, Phone, MapPin, RefreshCw, XCircle, Info } from 'lucide-react';
 
-const TermsOfUse: React.FC = () => {
+const TermsOfUse = () => {
   return (
     <div className="min-h-screen bg-gray-50 pt-24 pb-12">
       <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
