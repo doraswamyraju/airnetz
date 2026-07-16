b0b90b490d9a8a58a47db976baf26adadf2106cc
Doraswamy Raju Meesala <doraswamyrajumeesala@Doraswamys-MacBook-Air.local>
2026-07-16 16:49:14 +0530

    fix: export default and named React types to resolve named imports errors

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,24 +1,44 @@
-declare namespace React {
-  type ReactNode = any;
-  type FC<P = {}> = any;
-  type FormEvent<T = any> = any;
-  type ChangeEvent<T = any> = any;
-  type CSSProperties = any;
-  type RefObject<T> = any;
-  
-  function useState<T>(initialState: T | (() => T)): [T, (newState: T | ((prevState: T) => T)) => void];
-  function useEffect(effect: () => void | (() => void), deps?: any[]): void;
-  function useRef<T>(initialValue?: T): { current: T };
-  function useContext<T>(context: any): T;
-  function createContext<T>(defaultValue: T): any;
-}
-
 declare module 'react' {
-  export = React;
+  export const useState: any;
+  export const useEffect: any;
+  export const useRef: any;
+  export const useContext: any;
+  export const createContext: any;
+  export const memo: any;
+  export const useMemo: any;
+  export const useCallback: any;
+  export const StrictMode: any;
+  export type ReactNode = any;
+  export type FC<P = {}> = any;
+  export type FormEvent<T = any> = any;
+  export type ChangeEvent<T = any> = any;
+  export type CSSProperties = any;
+  export type RefObject<T> = any;
+  export type ComponentType<T = any> = any;
+
+  const React: {
+    useState: typeof useState;
+    useEffect: typeof useEffect;
+    useRef: typeof useRef;
+    useContext: typeof useContext;
+    createContext: typeof createContext;
+    memo: typeof memo;
+    useMemo: typeof useMemo;
+    useCallback: typeof useCallback;
+    StrictMode: typeof StrictMode;
+    FC: FC;
+    Component: any;
+  };
+  
+  export default React;
 }
 
 declare module 'react-dom/client' {
   export const createRoot: any;
+  const ReactDOM: {
+    createRoot: typeof createRoot;
+  };
+  export default ReactDOM;
 }
 
 declare module 'react-router-dom' {
